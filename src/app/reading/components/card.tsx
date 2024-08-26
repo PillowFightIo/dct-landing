9c1e80c1322c53aca6b8167890e9259343f3b321
Conrad Kreyling <conrad@kreyling.biz>
2024-08-26 11:26:42 -0400

    cards be flippin

--- src/app/reading/components/card.tsx
+++ src/app/reading/components/card.tsx
@@ -2,6 +2,7 @@
 import Image, { getImageProps, ImageLoaderProps } from 'next/image'
 import { useState } from 'react'
 import * as suits from '../lib/suits'
+import card_back_baked from '../../../../public/card-back-baked.png'
 import styles from "./card.module.css"
 
 export const getRandomInt = (max:number) => Math.floor(Math.random() * max);
@@ -11,24 +12,27 @@ export const getRandomEntry = (entries: any[]) => entries[getRandomInt(entries.l
 export default function Card() {
   const keys = Object.keys(suits.ImageMappings);
   const [selected, setSelected] = useState("");
+  const [flipped, setFlipped] = useState(false);
   if(!selected) {
     setSelected(getRandomEntry(keys));
   }
 
   return (
-    <div className={
-      `${styles.card} 
-       ${styles.unflipped}`}>
+    <div 
+      className={[styles.card, flipped ? styles.flipped : styles.unflipped].join(' ')}
+      onClick={e => setFlipped(!flipped)}
+    >
       <div className={styles.inner}>
-        <div className={styles.back}></div>
-        <div className={styles.face}>
         <Image
-          className={styles.radius}
+          className={`${styles.radius} ${styles.back}`}
+          src={card_back_baked}
+          fill
+          alt=""/>
+        <Image
+          className={`${styles.radius} ${styles.face}`}
           src={suits.ImageMappings[selected]}
           alt=""
           fill/>
-
-        </div>
       </div>
       <div className={styles.innerborder}></div>
     </div>
